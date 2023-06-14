9460808fec8289a5dc15dd75768d8c77b557214c
jooble-gtm-official <94429871+jooble-gtm-official@users.noreply.github.com>
2023-06-14 14:00:24 +0300

    Change new-baner (ASER)

--- src/teleporthq/components/new-banner.js
+++ src/teleporthq/components/new-banner.js
@@ -8,10 +8,16 @@ const NewBanner = (props) => {
   return (
     <div className={`new-banner-container ${props.rootClassName} `}>
       <span className="new-banner-text">New cool banner</span>
-      <span onClick={props.onClick1} className="new-banner-text1">
+      <a
+        href="https://www.google.com/"
+        target="_blank"
+        rel="noreferrer noopener"
+        onClick={props.onClick1}
+        className="new-banner-link"
+      >
         Send Statistics
-      </span>
-      <span onClick={props.onClick2} className="new-banner-text2">
+      </a>
+      <span onClick={props.onClick2} className="new-banner-text1">
         Send Statistics And Open Link
       </span>
     </div>
@@ -19,16 +25,16 @@ const NewBanner = (props) => {
 }
 
 NewBanner.defaultProps = {
+  onClick1: () => {},
   text: 'Ne',
   rootClassName: '',
-  onClick1: () => {},
   onClick2: () => {},
 }
 
 NewBanner.propTypes = {
+  onClick1: PropTypes.func,
   text: PropTypes.string,
   rootClassName: PropTypes.string,
-  onClick1: PropTypes.func,
   onClick2: PropTypes.func,
 }
 
