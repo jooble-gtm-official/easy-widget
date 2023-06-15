5f6ea2a7fdc36048516cfdc34fd84074da0d6f94
jooble-gtm-official <94429871+jooble-gtm-official@users.noreply.github.com>
2023-06-15 19:37:01 +0300

    add new component

---
+++ src/teleporthq/components/test-new-component.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './test-new-component.css'
+
+const TestNewComponent = (props) => {
+  return <div className="test-new-component-container"></div>
+}
+
+export default TestNewComponent
