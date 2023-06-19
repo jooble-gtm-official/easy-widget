29ec72b87d8c8f19afeebd5a878e118ccb517a29
jooble-gtm-official <94429871+jooble-gtm-official@users.noreply.github.com>
2023-06-19 17:28:41 +0300

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import { AppStateProvider } from './store' 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './teleporthq/style.css'
-import JayJayBanner from './teleporthq/components/jay-jay-banner'
 
 const App = () => {
   return (
-    <Router>
-      <AppStateProvider>
-        <Route component={JayJayBanner} exact path="/" />
-      </AppStateProvider>
-    </Router>
+    <Router/>
   )
 }
 
