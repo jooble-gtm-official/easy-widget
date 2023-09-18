db3bdea0321cbfb517d3e4b60fb92afcb479b529
jooble-gtm-official <94429871+jooble-gtm-official@users.noreply.github.com>
2023-09-18 14:17:27 +0300

    added teraz recruit banner

---
+++ src/teleporthq/components/teraz-recruit.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import './teraz-recruit.css'
+
+const TerazRecruit = (props) => {
+  return (
+    <div className="teraz-recruit-container">
+      <div className="teraz-recruit-frame1427">
+        <div className="teraz-recruit-frame1425">
+          <div className="teraz-recruit-img">
+            <img
+              src="/img31122-v5vp.png"
+              alt="img31122"
+              className="teraz-recruit-img31"
+            />
+          </div>
+          <div className="teraz-recruit-frame1421">
+            <div className="teraz-recruit-frame1418">
+              <span className="teraz-recruit-text MobileH2">
+                <span>Працюй рекрутером віддалено</span>
+              </span>
+              <span className="teraz-recruit-text2 ParagraphP16(default)">
+                <span>
+                  Заробляй до 8000 грн за кожного працевлаштованого кандидата
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <button className="teraz-recruit-buttons">
+          <span className="teraz-recruit-text4">
+            <span>Стати рекрутером</span>
+          </span>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default TerazRecruit
