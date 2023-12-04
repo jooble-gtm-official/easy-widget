a57bc3ce23693fd6031ceec73b7e8f0d159b3f67
Olexiy Kubliy <akub@jooble.com>
2023-12-04 15:28:02 +0200

    The entire area of the banner free-cv-en was made clickable.

--- src/teleporthq/components/free-cv-en.js
+++ src/teleporthq/components/free-cv-en.js
@@ -7,62 +7,59 @@ import './free-cv-en.css'
 const FreeCVEn = (props) => {
   return (
     <div className="free-cv-en-container">
-      <div className="free-cv-en-freecv4">
-        <div className="free-cv-en-frame1stimg">
-          <div className="free-cv-en-group207042">
-            <div className="free-cv-en-group207073">
-              <img
-                alt={props.IMAGE1stimg1_alt}
-                src={props.IMAGE1stimg1_src}
-                className="free-cv-en-image1stimg1"
-              />
+      <a
+        href={props.Freecv_btn}
+        target="_blank"
+        rel="noreferrer noopener"
+        className="free-cv-en-link"
+      >
+        <div onClick={props.Freecv_btnClick} className="free-cv-en-freecv4">
+          <div className="free-cv-en-frame1stimg">
+            <div className="free-cv-en-group207042">
+              <div className="free-cv-en-group207073">
+                <img
+                  alt={props.IMAGE1stimg1_alt}
+                  src={props.IMAGE1stimg1_src}
+                  className="free-cv-en-image1stimg1"
+                />
+              </div>
             </div>
+            <img
+              alt="Rectangle4448199"
+              src="/Rectangle4448199-udx8.svg"
+              className="free-cv-en-rectangle4448"
+            />
+            <span className="free-cv-en-text">
+              <span>with free AI tools</span>
+            </span>
           </div>
-          <img
-            alt="Rectangle4448199"
-            src="/Rectangle4448199-udx8.svg"
-            className="free-cv-en-rectangle4448"
-          />
-          <span className="free-cv-en-text">
-            <span>with free AI tools</span>
-          </span>
-        </div>
-        <div className="free-cv-en-group207055">
-          <span className="free-cv-en-text2">New job needs new CV</span>
-          <span className="free-cv-en-text3">
-            FreeCV builder + 10 minutes= your new professional CV
-          </span>
-        </div>
-        <a
-          href={props.Freecv_btn}
-          target="_blank"
-          rel="noreferrer noopener"
-          className="free-cv-en-link"
-        >
-          <div
-            onClick={props.Freecv_btnClick}
-            className="free-cv-en-frameprimarydefaultm"
-          >
+          <div className="free-cv-en-group207055">
+            <span className="free-cv-en-text2">New job needs new CV</span>
+            <span className="free-cv-en-text3">
+              FreeCV builder + 10 minutes= your new professional CV
+            </span>
+          </div>
+          <div className="free-cv-en-frameprimarydefaultm">
             <span className="free-cv-en-text4">Create my CV</span>
           </div>
-        </a>
-      </div>
+        </div>
+      </a>
     </div>
   )
 }
 
 FreeCVEn.defaultProps = {
-  Freecv_btnClick: () => {},
   IMAGE1stimg1_src: '/IMAGE1stimg1431-dli5.png',
   IMAGE1stimg1_alt: 'IMAGE1stimg1431',
   Freecv_btn: '',
+  Freecv_btnClick: () => {},
 }
 
 FreeCVEn.propTypes = {
-  Freecv_btnClick: PropTypes.func,
   IMAGE1stimg1_src: PropTypes.string,
   IMAGE1stimg1_alt: PropTypes.string,
   Freecv_btn: PropTypes.string,
+  Freecv_btnClick: PropTypes.func,
 }
 
 export default FreeCVEn
