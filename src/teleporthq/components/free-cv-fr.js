05f0f2693750c6453a1813be4dba9d34360c2658
Olexiy Kubliy <akub@jooble.com>
2023-12-04 15:08:08 +0200

    The entire area of the banner free-cv-fr was made clickable.

--- src/teleporthq/components/free-cv-fr.js
+++ src/teleporthq/components/free-cv-fr.js
@@ -7,43 +7,43 @@ import './free-cv-fr.css'
 const FreeCVFr = (props) => {
   return (
     <div className="free-cv-fr-container">
-      <div onClick={props.Freecv_btnClick} className="free-cv-fr-freecv4">
-        <div className="free-cv-fr-frame1stimg">
-          <div className="free-cv-fr-group207042">
-            <div className="free-cv-fr-group207073">
-              <img
-                alt={props.IMAGE1stimg1_alt}
-                src={props.IMAGE1stimg1_src}
-                className="free-cv-fr-image1stimg1"
-              />
+      <a
+        href={props.Freecv_btn}
+        target="_blank"
+        rel="noreferrer noopener"
+        className="free-cv-fr-link"
+      >
+        <div onClick={props.Freecv_btnClick} className="free-cv-fr-freecv4">
+          <div className="free-cv-fr-frame1stimg">
+            <div className="free-cv-fr-group207042">
+              <div className="free-cv-fr-group207073">
+                <img
+                  alt={props.IMAGE1stimg1_alt}
+                  src={props.IMAGE1stimg1_src}
+                  className="free-cv-fr-image1stimg1"
+                />
+              </div>
             </div>
+            <img
+              alt="Rectangle4448199"
+              src="/Rectangle4448199-udx8.svg"
+              className="free-cv-fr-rectangle4448"
+            />
+            <span className="free-cv-fr-text">Aide d&apos;AI Inclus</span>
           </div>
-          <img
-            alt="Rectangle4448199"
-            src="/Rectangle4448199-udx8.svg"
-            className="free-cv-fr-rectangle4448"
-          />
-          <span className="free-cv-fr-text">Aide d&apos;AI Inclus</span>
-        </div>
-        <div className="free-cv-fr-group207055">
-          <span className="free-cv-fr-text1">
-            Un nouvel emploi nécessite un nouveau CV
-          </span>
-          <span className="free-cv-fr-text2">
-            FreeCV builder + 10 minutes = votre nouveau CV professionnel
-          </span>
-        </div>
-        <a
-          href={props.Freecv_btn}
-          target="_blank"
-          rel="noreferrer noopener"
-          className="free-cv-fr-link"
-        >
+          <div className="free-cv-fr-group207055">
+            <span className="free-cv-fr-text1">
+              Un nouvel emploi nécessite un nouveau CV
+            </span>
+            <span className="free-cv-fr-text2">
+              FreeCV builder + 10 minutes = votre nouveau CV professionnel
+            </span>
+          </div>
           <div className="free-cv-fr-frameprimarydefaultm">
             <span className="free-cv-fr-text3">Créer mon CV</span>
           </div>
-        </a>
-      </div>
+        </div>
+      </a>
     </div>
   )
 }
